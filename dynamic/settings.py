e3c5a71bf339093a88d9550bfb16e01c9c24eb91
olega211 <106370448+olega211@users.noreply.github.com>
2022-10-12 21:57:10 +0700

    path driver

--- dynamic/settings.py
+++ dynamic/settings.py
@@ -1,6 +1,6 @@
 import os
 
-# DRIVER = '/usr/lib/chromium-browser/chromedriver'
-DRIVER = 'chromedriver'
+DRIVER = '/usr/lib/chromium-browser/chromedriver'
+# DRIVER = 'chromedriver'
 
 EXT_PATH = os.path.abspath('extension_0_15_1_0.crx')
